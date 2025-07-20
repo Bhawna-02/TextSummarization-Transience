7bf8129c267aa99551c4e9b2c105741d47f9519c
Bhawna-02 <114484895+Bhawna-02@users.noreply.github.com>
2025-07-20 13:58:50 +0530

    Add files via upload

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from tr_model import clean_input, summarize_text  
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class PromptRequest(BaseModel):
+    prompt: str
+
+@app.post("/api/transience")
+def transience_api(data: PromptRequest):
+    cleaned_prompt = clean_input(data.prompt)
+    result = summarize_text(cleaned_prompt)
+    return {"response": result}
